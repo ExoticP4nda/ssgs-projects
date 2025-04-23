d2980ee5a31880efc1c9c3e889baf822206e7f08
ExoticP4nda <Robertors98@libero.it>
2025-04-23 14:19:54 +0200

    Laboratorio 2: prima versione

---
+++ ssgs-calc/test/math.test.js
@@ -0,0 +1,13 @@
+const math = require('../math');
+
+// Test corretto
+test('2 + 2 dovrebbe essere 4', () => {
+    const result = math.addizione(2, 2);
+    expect(result).toBe(4);
+});
+
+// Test sbagliato (fallirà)
+test('3 + 5 dovrebbe essere 7 (test sbagliato)', () => {
+    const result = math.addizione(3, 5);
+    expect(result).toBe(7);
+});
