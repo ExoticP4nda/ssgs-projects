e2354d6c2c70dab9e2e69c71a7e101d547c205c9
ExoticP4nda <Robertors98@libero.it>
2025-04-23 15:33:35 +0200

    Igrona test fallito

--- ssgs-calc/test/math.test.js
+++ ssgs-calc/test/math.test.js
@@ -7,7 +7,7 @@ test('2 + 2 dovrebbe essere 4', () => {
 });
 
 // Test sbagliato (fallirà)
-test.skip('3 + 5 dovrebbe essere 7 (test sbagliato)', () => {
+test.skip('3 + 5 dovrebbe essere 7(test sbagliato)', () => {
     const result = math.addizione(3, 5);
     expect(result).toBe(7);
 });
